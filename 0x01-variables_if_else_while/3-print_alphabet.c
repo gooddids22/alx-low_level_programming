22ac6b5d3db8e5cbee8495fe55921c19eba94a98
gooddids22 <splendidotamiri@gmail.com>
2023-03-17 10:06:34 -0700

    3-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
