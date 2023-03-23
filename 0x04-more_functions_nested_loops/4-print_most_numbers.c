9d36ea7d78d70000710f65946fdd9c8f73f83c6a
gooddids22 <splendidotamiri@gmail.com>
2023-03-23 11:30:39 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Program function to print num  0 - 9 except 2 and 4
+ * print_most_numbers - Program function to print num 0 - 9 except 2 and 4
  *
  * return: void
  */
