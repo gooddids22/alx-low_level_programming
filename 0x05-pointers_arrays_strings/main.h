6b0f947e34b523c6f061c700453d403f460437a6
gooddids22 <splendidotamiri@gmail.com>
2023-03-27 04:25:26 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -7,3 +13,7 @@ int _strlen(char *s);
 void _puts(char *str);
 
 int _putchar(char c);
+
+void print_rev(char *s);
+
+#endif
