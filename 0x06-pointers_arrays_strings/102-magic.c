b05ab558a15aace671b3c85945a2a469aee3ce44
gooddids22 <splendidotamiri@gmail.com>
2023-03-30 04:25:41 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,16 +8,20 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-p[5] = 98;
-  /* ...so that this prints 98\n */
+/*
+* main: Entry point
+* Description: write one line of code that prints a[2] = 98
+*
+* @n: parameter
+* Return: 0 (Success)
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
