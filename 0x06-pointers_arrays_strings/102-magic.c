9f5ee4157f46be00496e929bbbe235fe3820b8c9
gooddids22 <splendidotamiri@gmail.com>
2023-03-30 05:45:26 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,8 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* write your line of code here..i.
+ * main - prints a number on a new line
+* write your line of code here...
 * Remember:
 * - you are not allowed to use a
 * - you are not allowed to modify p
