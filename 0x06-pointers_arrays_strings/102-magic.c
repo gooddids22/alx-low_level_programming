878e12b00f358451849ea52131df9cf69ece5790
gooddids22 <splendidotamiri@gmail.com>
2023-03-30 05:52:25 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,6 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
- * main - prints a number on a new line
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
@@ -17,7 +16,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
+p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
