5633917512dd575571d32b3f52b603e654a8c219
gooddids22 <splendidotamiri@gmail.com>
2023-04-03 06:10:43 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,12 +5,14 @@
  * _strspn - a function that obtains the length of a substring
  * @s: String
  * @accept: a string that contains characters in s
- * Return: count
+ * Return: return count
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
+
 	int i, j;
+
 	int count = 0;
 	char *str1, *str2;
 
@@ -18,28 +20,27 @@ unsigned int _strspn(char *s, char *accept)
 	str2 = accept;
 
 	i = 0;
-	while (str1[i] != '\0')
+	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
 		j = 0;
-		while (str2[j] != '\0')
-
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
 		{
-			if (str2[j] == str1[i])
+			if (str2[j] == str1[i]) /*Evaluate condition*/
 			{
-				count++;
+				count++; /*count number*/
 				break;
 			}
 
-			j++;
+			j++;    /*add j+1*/
 		}
 
-		if (s[i] != accept[j])
+		if (s[i] != accept[j]) /*If aren't equals*/
 		{
-
+			break;
 		}
 
-		i++;
+		i++; /*add x+1*/
 	}
 
-	return (count);
+	return (count); /*return the value of count*/
 }
