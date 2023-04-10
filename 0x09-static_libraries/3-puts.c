54190e79d0b4b9e8d093919b4f46d521d45c9f3f
gooddids22 <splendidotamiri@gmail.com>
2023-04-09 18:27:53 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+* _puts - function that inputs character to string
+*
+* Return: Always 0 (Success)
+*/
+
+void _puts(char *str)
+
+int main(void);
+
+{
+
+_puts("\"Programming is like building a multilingual puzzle");
+return 0;
+}
