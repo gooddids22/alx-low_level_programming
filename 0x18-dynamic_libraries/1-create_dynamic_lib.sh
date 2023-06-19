d1392e69ab567d180183d09358b76bb6450a3588
gooddids22 <splendidotamiri@gmail.com>
2023-06-19 13:30:57 +0100

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.c
-export LD_LIBRARY_PATH=. :$LD_LIBRARY_PATH
+gcc -c -fpic *.c
+gcc -shared -o liball.so *.o
